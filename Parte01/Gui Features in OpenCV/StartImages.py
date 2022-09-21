a9e785a49981fd0a4500b06a1e06fac867bec45d
Tatiana <tatianaresende@ua.pt>
2022-09-21 18:57:11 +0100

    Getting Started with Images

---
+++ Parte01/Gui Features in OpenCV/StartImages.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+#The OpenCV python library is imported
+from turtle import clear
+import cv2 as cv
+import sys
+
+#Initialization:
+img = cv.imread(cv.samples.findFile("starry_night.jpg"))
+
+#Execution:
+#Check if the image was load correctly
+if img is None: 
+    sys.exit("Could not read the image.")
+
+cv.imshow("Display window", img)
+k = cv.waitKey(0)
+
+if k == ord("s"):
+    cv.imwrite("starry_night.png", img)
