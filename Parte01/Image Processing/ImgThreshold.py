dd9d0240da3f3c1e63ffa6c4bee09a8b739d55b7
Tatiana <tatianaresende@ua.pt>
2022-09-26 18:52:52 +0100

    Tutotial 4.3

--- Parte01/Image Processing/ImgThreshold.py
+++ Parte01/Image Processing/ImgThreshold.py
@@ -46,3 +46,73 @@ for i in range(4):
     plt.title(titles[i])
     plt.xticks([]),plt.yticks([])
 plt.show()
+
+# -------------------------------------------------------------
+#Otsu's Binarization 
+imgN = cv.imread('noisy2.png',0)
+
+# global thresholding
+ret1,thr1 = cv.threshold(imgN,127,255,cv.THRESH_BINARY)
+
+# Otsu's thresholding
+ret2,thr2 = cv.threshold(imgN,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+
+# Otsu's thresholding after Gaussian filtering
+blur = cv.GaussianBlur(imgN,(5,5),0)
+ret3,thr3 = cv.threshold(blur,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+
+# plot all the images and their histograms
+images = [imgN, 0, thr1,
+          imgN, 0, thr2,
+          blur, 0, thr3]
+titles = ['Original Noisy Image','Histogram','Global Thresholding (v=127)',
+          'Original Noisy Image','Histogram',"Otsu's Thresholding",
+          'Gaussian filtered Image','Histogram',"Otsu's Thresholding"]
+
+plt.figure(3)       
+for i in range(3):
+    plt.subplot(3,3,i*3+1),plt.imshow(images[i*3],'gray')
+    plt.title(titles[i*3]), plt.xticks([]), plt.yticks([])
+
+    plt.subplot(3,3,i*3+2),plt.hist(images[i*3].ravel(),256)
+    plt.title(titles[i*3+1]), plt.xticks([]), plt.yticks([])
+    
+    plt.subplot(3,3,i*3+3),plt.imshow(images[i*3+2],'gray')
+    plt.title(titles[i*3+2]), plt.xticks([]), plt.yticks([])
+plt.show()
+
+
+# -------------------------------------------------------------
+#How does Otsu's Binarization work?
+img = cv.imread('noisy2.png',0)
+blur = cv.GaussianBlur(img,(5,5),0)
+
+# find normalized_histogram, and its cumulative distribution function
+hist = cv.calcHist([blur],[0],None,[256],[0,256])
+hist_norm = hist.ravel()/hist.sum()
+
+Q = hist_norm.cumsum()
+bins = np.arange(256)
+fn_min = np.inf
+thresh = -1
+
+for i in range(1,256):
+    p1,p2 = np.hsplit(hist_norm,[i]) # probabilities
+    q1,q2 = Q[i],Q[255]-Q[i] # cum sum of classes
+    if q1 < 1.e-6 or q2 < 1.e-6:
+        continue
+    b1,b2 = np.hsplit(bins,[i]) # weights
+
+    # finding means and variances
+    m1,m2 = np.sum(p1*b1)/q1, np.sum(p2*b2)/q2
+    v1,v2 = np.sum(((b1-m1)**2)*p1)/q1,np.sum(((b2-m2)**2)*p2)/q2
+
+    # calculates the minimization function
+    fn = v1*q1 + v2*q2
+    if fn < fn_min:
+        fn_min = fn
+        thresh = i
+        
+# find otsu's threshold value with OpenCV function
+ret, otsu = cv.threshold(blur,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+print( "{} {}".format(thresh,ret) )
