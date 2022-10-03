255ff04791fbf0ceb81abd7f89e769cde4352e61
Tatiana <tatianaresende@ua.pt>
2022-10-03 18:49:41 +0100

    4

--- Parte03/Exc1.py
+++ Parte03/Exc1.py
@@ -1,27 +1,25 @@
 #!/usr/bin/env python3
 
 import cv2
-#from matplotlib.pyplot import imshow
 import numpy as np
 from time import sleep
 
 #Constants
-widh_min = 100; widht_max = 400  # rectangle widh
-height_min = 100; height_max = 400  # rectangle height
+width_min = 100; # rectangle width
+height_min = 100; # rectangle height
 offset = 6  # Allowed error between pixels
 pos_line = 600  # Count line position
 delay = 60  # FPS do vídeo
 detec = []
-line_x1 = 180
-line_x2 = 1160
+line_x1 = 150; line_x2 = 1160
 
-def pega_centro(x, y, widh, height):
+def find_center(x, y, widh, height):
     """
-    :param x: x do objeto
-    :param y: y do objeto
-    :param widh: largura do objeto
-    :param height: altura do objeto
-    :return: dupla que contém as coordenadas do centro de um objeto
+    :param x: x of the object
+    :param y: y of the object
+    :param width: object width
+    :param height: object height
+    :return: double that contains the coordinates of the center of an object
     """
     x1 = widh // 2
     y1 = height // 2
@@ -30,61 +28,59 @@ def pega_centro(x, y, widh, height):
     return cx, cy
 
 def set_info(detec):
-    global carros
+    global cars
     for (x, y) in detec:
         if (pos_line + offset) > y > (pos_line - offset):
-            carros += 1
+            cars += 1
             cv2.line(frame1, (line_x1, pos_line), (line_x2, pos_line), (0, 127, 255), 3)
             detec.remove((x, y))
-            print("Carros detectados até o momento: " + str(carros))
+            print("Carros detectados até o momento: " + str(cars))
 
 def show_info(frame1, img_work):
-    text = f'Carros: {carros}'
+    text = f'Carros: {cars}'
     cv2.putText(frame1, text, (450, 70), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 5)
     cv2.imshow("Video Original", frame1)
     cv2.imshow("Detectar", img_work)
 
-carros = 0
+cars = 0
 cap = cv2.VideoCapture('./docs/traffic.mp4')
-subtracao = cv2.bgsegm.createBackgroundSubtractorMOG()  # Pega o fundo e subtrai do que está se movendo
+subtracao = cv2.bgsegm.createBackgroundSubtractorMOG()  # Take the background and subtract from what's moving
 
 while True:
-    ret, frame1 = cap.read()  # Pega cada frame do vídeo
+    ret, frame1 = cap.read()  # Grab every frame of the video
     tempo = float(1 / delay)
-    sleep(tempo)  # Dá um delay entre cada processamento
+    sleep(tempo)  # Delay between each processing
 
-    grey = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)  # Pega o frame e transforma para preto e branco
-    blur = cv2.GaussianBlur(grey, (3, 3), 5)  # Faz um blur para tentar remover as imperfeições da imagem
+    grey = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)  # RGB to GRAY
+    blur = cv2.GaussianBlur(grey, (3, 3), 5)  # image smoothing
     
-    img_sub = subtracao.apply(blur)  # Faz a subtração da imagem aplicada no blur
+    img_sub = subtracao.apply(blur)  # Subtraction of the image applied in the blur
    
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (
-        5, 5))  # Cria uma matriz 5x5, em que o formato da matriz entre 0 e 1 forma uma elipse dentro
+        5, 5))  # 5x5 matrix, where the matrix format between 0 and 1 forms an ellipse inside
     element = np.ones((5,5))
 
     img_work = img_sub
     img_work = cv2.erode(img_work, element)
-    img_work = cv2.morphologyEx(img_work, cv2.MORPH_CLOSE, kernel)  # Tenta preencher todos os "buracos" da imagem
+    img_work = cv2.morphologyEx(img_work, cv2.MORPH_CLOSE, kernel)  # fill in holes
        
-    contorno, img = cv2.findContours(img_work, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contour, img = cv2.findContours(img_work, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     cv2.line(frame1, (line_x1, pos_line), (line_x2, pos_line), (255, 127, 0), 3)
     cv2.line(img_work, (line_x1, pos_line), (line_x2, pos_line), (255, 127, 0), 3)
     
-    for (i, c) in enumerate(contorno):
+    for (i, c) in enumerate(contour):
         (x, y, w, h) = cv2.boundingRect(c)
-        validar_contorno = (w >= widh_min) and (h >= height_min)
-        if not validar_contorno:
+        validate_contorno = (w >= width_min) and (h >= height_min)
+        if not validate_contorno:
             continue
 
         cv2.rectangle(frame1, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        cv2.rectangle(img_work, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        centro = pega_centro(x, y, w, h)
-        detec.append(centro)
-        cv2.circle(frame1, centro, 4, (0, 0, 255), -1)
-        cv2.circle(img_work, centro, 4, (0, 0, 255), -1)
+        center = find_center(x, y, w, h)
+        detec.append(center)
+        cv2.circle(frame1, center, 4, (0, 0, 255), -1)
+
 
     set_info(detec)
-    #show_info(img_work, img_work)
     show_info(frame1, img_work)
 
     if cv2.waitKey(1) == 27:
