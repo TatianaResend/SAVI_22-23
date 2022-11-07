f22da831cd5c5c1c77755c744db94d1325da53c1
Tatiana <tatianaresende@ua.pt>
2022-11-07 14:12:52 +0000

    updates

--- Parte06/Exc1.py
+++ Parte06/Exc1.py
@@ -5,18 +5,27 @@ import cv2 as cv
 CornerHarris = False
 Shi_Tomasi = False
 SIFT = False
+SURF = False
+FAST = False
+BRIEF = False
 
 def main():
     # select image:
     #filename = 'chessboard.png'
     #filename = 'blox.jpg'
-    filename = 'home.png'
+    #filename = 'home.png'
+    #filename = 'fly.png'
+    filename = 'simple.png'
+
 
     # ---------------------------------------------------------------------
     # select method:
-    # CornerHarris = True
-    # Shi_Tomasi = True
-    SIFT = True
+    #CornerHarris = True
+    #Shi_Tomasi = True
+    #SIFT = True
+    #SURF = True
+    #FAST = True
+    BRIEF = True
 
     img = cv.imread(filename)
     gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
@@ -51,7 +60,49 @@ def main():
         #img=cv.drawKeypoints(gray,kp,img)
         img=cv.drawKeypoints(gray,kp,img,flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
         print('SIFT')
-    
+
+    if SURF:
+        #faster than SIFT
+        # Create SURF object. You can specify params here or later.
+        # Here I set Hessian Threshold to 400
+        surf = cv.xfeatures2d.SURF_create(400)
+        # Find keypoints and descriptors directly
+        kp, des = surf.detectAndCompute(img,None)
+        print('SURF')
+        print(len(kp))
+
+    if FAST:
+        # Initiate FAST object with default values
+        fast = cv.FastFeatureDetector_create()
+        # find and draw the keypoints
+        kp = fast.detect(img,None)
+        img2 = cv.drawKeypoints(img, kp, None, color=(255,0,0))
+        # Print all default params
+        print( "Threshold: {}".format(fast.getThreshold()) )
+        print( "nonmaxSuppression:{}".format(fast.getNonmaxSuppression()) )
+        print( "neighborhood: {}".format(fast.getType()) )
+        print( "Total Keypoints with nonmaxSuppression: {}".format(len(kp)) )
+        cv.imwrite('fast_true.png', img2)
+        # Disable nonmaxSuppression
+        fast.setNonmaxSuppression(0)
+        kp = fast.detect(img, None)
+        print( "Total Keypoints without nonmaxSuppression: {}".format(len(kp)) )
+        img3 = cv.drawKeypoints(img, kp, None, color=(255,0,0))
+        cv.imwrite('fast_false.png', img3)
+
+    if BRIEF:
+        # Initiate FAST detector
+        star = cv.xfeatures2d.StarDetector_create()
+        # Initiate BRIEF extractor
+        brief = cv.xfeatures2d.BriefDescriptorExtractor_create()
+        # find the keypoints with STAR
+        kp = star.detect(img,None)
+        # compute the descriptors with BRIEF
+        kp, des = brief.compute(img, kp)
+        print( brief.descriptorSize() )
+        print( des.shape )
+
+       
 
     
     cv.imshow('dst',img)
--- Parte06/Exc1_aula.py
+++ Parte06/Exc1_aula.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 
 import numpy as np
-import cv2 as cv
-from matplotlib import pyplot as plt
+import cv2
+print(cv2.__version__)
+# from matplotlib import pyplot as plt
 
-def main():
-    img = cv.imread('blox.jpg')
-    gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-    corners = cv.goodFeaturesToTrack(gray,25,0.01,10)
+# def main():
+#     img = cv2.imread('blox.jpg')
+#     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+#     corners = cv2.goodFeaturesToTrack(gray,25,0.01,10)
     
-    corners = np.int0(corners)
-    for i in corners:
-        x,y = i.ravel()
-        cv.circle(img,(x,y),3,255,-1)
-    plt.imshow(img),plt.show()
+#     corners = np.int0(corners)
+#     for i in corners:
+#         x,y = i.ravel()
+#         cv2.circle(img,(x,y),3,255,-1)
+#     plt.imshow(img),plt.show()
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
