a5b5826de4bd85693d60753a5b56ebccf3f07edd
Tatiana <tatianaresende@ua.pt>
2022-12-04 16:27:24 +0000

    Class 9

--- Parte09/Exc1.py
+++ Parte09/Exc1.py
@@ -3,8 +3,30 @@
 import open3d as o3d
 import numpy as np
 
+view = {
+	"class_name" : "ViewTrajectory",
+	"interval" : 29,
+	"is_loop" : False,
+	"trajectory" : 
+	[
+		{
+			"boundingbox_max" : [ 6.5291471481323242, 34.024543762207031, 11.225864410400391 ],
+			"boundingbox_min" : [ -39.714397430419922, -16.512752532958984, -1.9472264051437378 ],
+			"field_of_view" : 60.0,
+			"front" : [ 0.54907281448319933, -0.72074094308345071, 0.42314481842352314 ],
+			"lookat" : [ -7.4165150225483982, -4.3692552972898397, 4.2418377265036487 ],
+			"up" : [ -0.27778678941340029, 0.3201300269334113, 0.90573244696378663 ],
+			"zoom" : 0.26119999999999988
+		}
+	],
+	"version_major" : 1,
+	"version_minor" : 0
+}
+
 
 def main():
+
+
 
     # ------------------------------------------
     # Initialization
@@ -12,13 +34,18 @@ def main():
     print("Load a ply point cloud, print it, and render it")
     #ply_point_cloud = o3d.data.PLYPointCloud()
     point_cloud = o3d.io.read_point_cloud('data/Factory/factory.ply')
-    print(point_cloud)
-    print(np.asarray(point_cloud.points))
+    #print(point_cloud)
+    #print(np.asarray(point_cloud.points))
+    
+    print(view)
+    print(view["trajectory"][0]["zoom"])
+  
     o3d.visualization.draw_geometries([point_cloud],
-                                    zoom=0.3412,
-                                    front=[0.4257, -0.2125, -0.8795],
-                                    lookat=[2.6172, 2.0475, 1.532],
-                                    up=[-0.0694, -0.9768, 0.2024])
+                                    zoom=view['trajectory'][0]['zoom'],
+                                    front=view['trajectory'][0]['front'],
+                                    lookat=view['trajectory'][0]['lookat'],
+                                    up=view['trajectory'][0]['up'])
+
 
     # ------------------------------------------
     # Execution
